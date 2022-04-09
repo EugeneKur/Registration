26b2e429799fafd18f4fe7d663213b3e10866c48
EugeneKur <ew.kurilov@gmail.com>
2022-04-09 11:49:20 +0300

    Переделал контракт под MVVM. Старый код на время закомментировал, чтобы можно было посмотреть ка было раньше

--- app/src/main/java/ru/geekbrains/registration/mvp/ui/login/RegistrationContracts.kt
+++ app/src/main/java/ru/geekbrains/registration/mvp/ui/login/RegistrationContracts.kt
@@ -5,25 +5,28 @@ import androidx.annotation.MainThread
 
 class RegistrationContracts {
 
-    interface View {
-        @MainThread
-        fun setSuccess()
-
-        @MainThread
-        fun setError(error: String)
-
-        @MainThread
-        fun showProgress()
-
-        @MainThread
-        fun hideProgress()
-        fun showLogin()
-        fun showPassword()
-        fun getHandler(): Handler
-    }
+//    interface View {
+//        @MainThread
+//        fun setSuccess()
+//
+//        @MainThread
+//        fun setError(error: String)
+//
+//        @MainThread
+//        fun showProgress()
+//
+//        @MainThread
+//        fun hideProgress()
+//        fun showLogin()
+//        fun showPassword()
+//        fun getHandler(): Handler
+//    }
 
     interface Presenter {
-        fun onAttach(view: View)
+        var shouldShowProgress: Boolean
+        var isSuccess: Boolean
+        var error: String?
+
         fun onLogin(login: String, password: String)
         fun onCredentialsChange()
     }
